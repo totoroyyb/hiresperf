24acbb0ded8956eeb4aaf99cac898c7050193f38
ZoePengHouse <zixpeng@outlook.com>
2025-10-14 01:46:40 +0000

    Add: commented counters

--- src/config.h
+++ src/config.h
@@ -84,7 +84,7 @@ static const unsigned long hrp_pmc_cpu_selection_mask_bits[HRP_PMC_CPU_SELECTION
 #define HRP_USE_STALL_TOTAL 1
 #define HRP_USE_BOUND_ON_LOADS 0
 #define HRP_USE_BOUND_ON_STORES 0
-#define HRP_USE_COUNTER_COMBINATION 1 // set to 1 to use counter combination feature, 0 to disable
+#define HRP_USE_COUNTER_COMBINATION 1 // set to 1 to use counter combination feature, 0 to disable, new counters are stall total, L1D miss, L2 miss, L3 miss
 
 #define HRP_USE_RDT     0 // set to 1 to use RDT events (MBM, CMT), 0 to disable
 /*
